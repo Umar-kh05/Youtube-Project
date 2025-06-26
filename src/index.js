e5650044a303e942bd0b16d4c6513c7760c2e66b
Umar-kh05 <umarshoaib66@gmail.com>
2025-06-26 14:15:05 +0500

    API ERROR and Response handling

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path :'./.env'
+})
+
+connectDB()
+.then(() =>
+{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is runing at port ${process.env.PORT}`);
+    } )
+}
+)
+.catch( (err) => {
+    console.log("MONGO DB connection failed!! ", err)   
+})
+
+
+// import express from "express"
+// const app = express()
+
+// ;( async () =>
+// {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       
+//         app.on("Error", (error) => {
+//             console.error("Error : ", error)
+//             throw err
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on Port ${process.env.PORT}`);
+//         })
+        
+//     } catch (error) {
+//         console.error("ERROR: ", error)
+//         throw err
+        
+//     }
+// } )()
---
+++ src/utils/assyncHandler.js
@@ -0,0 +1,8 @@
+const assynchandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+    }
+}
+
+export {assynchandler}
+
