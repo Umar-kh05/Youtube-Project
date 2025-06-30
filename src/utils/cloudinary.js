9eba0c4b893762a25a95cd9fce985f1fe93daad4
Umar-kh05 <umarshoaib66@gmail.com>
2025-06-30 17:02:20 +0500

    Finalizing professional setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,8 @@
+import { v2 as cloudinary} from "cloudinary";
+import fs from "fs"
+
+cloudinary.config({ 
+        cloud_name: 'dmocfbgpz', 
+        api_key: '222758239263379', 
+        api_secret: 'grbjebgiebirw12njkf' // Click 'View API Keys' above to copy your API secret
+    });
